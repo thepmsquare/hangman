6d5552f6f8747ee3b644a2c24c5eb87a128dad9a
thePmSquare <impmmmm@gmail.com>
2020-07-13 04:13:52 +0530

    Updates

---
+++ precache-manifest.e8a3b95c6a81fc9c8424bad8a67475b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5c8371648ec2aac0c13670c348134b",
+    "url": "/hangman-game-react/index.html"
+  },
+  {
+    "revision": "a0595f4e1dd4a588aa20",
+    "url": "/hangman-game-react/static/css/main.0970030c.chunk.css"
+  },
+  {
+    "revision": "7487587b22e46c164b62",
+    "url": "/hangman-game-react/static/js/2.c1722d6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-game-react/static/js/2.c1722d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0595f4e1dd4a588aa20",
+    "url": "/hangman-game-react/static/js/main.424f94ad.chunk.js"
+  },
+  {
+    "revision": "0a4a5f83061e21403226",
+    "url": "/hangman-game-react/static/js/runtime-main.1e588c88.js"
+  },
+  {
+    "revision": "90cc5114a89766ae940b8931028cea98",
+    "url": "/hangman-game-react/static/media/6.90cc5114.jpg"
+  }
+]);
