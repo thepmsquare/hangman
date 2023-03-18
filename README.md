3f701fff9e826579a6fdccfdaf545656f15eacd9
Parth Mangtani <impmmmm@gmail.com>
2023-03-18 13:06:09 +0530

    initial commit for hangman2

---
+++ README.md
@@ -0,0 +1,12 @@
+# hangman
+
+![](hangman.gif)
+
+Feedback is appreciated. Thank you!
+
+# built on
+
+- NodeJS v18.14.2
+- npm v9.5.0
+
+and more inside `package.json`
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./stylesheets/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hangman</div>
+    </div>
+  );
+}
+
+export default App;
